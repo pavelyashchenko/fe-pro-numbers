ad7064ac5f7bf337c016776ff91c8e7652aa4e2a
Pavel Yashchenko <yashchenkops@gmail.com>
2022-09-18 21:26:10 +0300

    homework done

--- src/index.js
+++ src/index.js
@@ -7,7 +7,12 @@
  * @param {number} maxSecond
  * @returns {number}
  */
-export const minMaxMultiply = (minFirst, minSecond, maxFirst, maxSecond) => {};
+export const minMaxMultiply = (minFirst, minSecond, maxFirst, maxSecond) => {
+  const minNum = Math.min(minFirst, minSecond);
+  const maxNum = Math.max(maxFirst, maxSecond);
+
+  return minNum * maxNum;
+};
 
 /**
  * Функция принимает один числовой аргумент, это радиус круга. Функция должна
@@ -16,7 +21,9 @@ export const minMaxMultiply = (minFirst, minSecond, maxFirst, maxSecond) => {};
  * @param {number} radius
  * @returns {number}
  */
-export const getSquareOfCircle = (radius) => {};
+export const getSquareOfCircle = (radius) => {
+  return Math.PI * radius ** 2;
+};
 
 /**
  * Функция принимает число и точность к которой нужно округлить
@@ -26,7 +33,9 @@ export const getSquareOfCircle = (radius) => {};
  * @param {number} accuracy
  * @returns {number}
  */
-export const getFixedValue = (number, accuracy) => {};
+export const getFixedValue = (number, accuracy) => {
+  return +number.toFixed(Math.round(accuracy));
+};
 
 /**
  * Если целая часть числа четная, то срабатывает Math.floor для этого числа
@@ -34,7 +43,15 @@ export const getFixedValue = (number, accuracy) => {};
  * @param {number} number
  * @returns {number}
  */
-export const getFlooroCeil = (number) => {};
+export const getFlooroCeil = (number) => {
+  const num = number.toFixed();
+
+  if (num % 2 === 0) {
+    return Math.floor(number);
+  } else {
+    return Math.ceil(number);
+  }
+};
 
 /**
  * Мы передаем два числа, от какого и до какого числа и функция должна
@@ -44,4 +61,9 @@ export const getFlooroCeil = (number) => {};
  * @param {number} to
  * @returns {number}
  */
-export const randomizer = (from, to) => {};
+export const randomizer = (from, to) => {
+  const minNum = Math.ceil(from);
+  const maxNum = Math.floor(to);
+
+  return Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+};
